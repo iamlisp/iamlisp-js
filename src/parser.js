2ce07b65dbe3d9f7a65a8e1d4b2997a78ad80a09
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-12-21 23:00:33 +0200

    Initial commit

---
+++ src/parser.js
@@ -0,0 +1,11 @@
+const parseSymbol = (code, offset) => {};
+
+const parseList = (code, offset) => {};
+
+const parseMap = (code, offset) => {};
+
+const parseString = (code, offset) => {};
+
+const parseQuotedString = (code, offset) => {};
+
+const parseInlineComment = (code, offset) => {};
