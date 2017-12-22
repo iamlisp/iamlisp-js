022e715d3f92766b1cbac8ef569d2c73ab420638
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-12-22 16:38:17 +0200

    Refactoring binary operators.

--- src/forms/binops.js
+++ src/forms/binops.js
@@ -1,10 +1,28 @@
 const SpecialForm = require('./SpecialForm');
 
 const binOp = (op, evaluate, args) => args.map(evaluate).reduce(op);
+const binCmp = (cmp, evaluate, args) => {
+  let leftOperand = evaluate(args.shift());
+  while (args.length > 0) {
+    const rightOperand = evaluate(args.shift());
+    if (!cmp(leftOperand, rightOperand)) {
+      return false;
+    }
+    leftOperand = rightOperand;
+  }
+  return true;
+}
 
 module.exports = {
-  '+': new SpecialForm((env, evaluate, args) => binOp((x, y) => x + y, evaluate, args)),
-  '*': new SpecialForm((env, evaluate, args) => binOp((x, y) => x * y, evaluate, args)),
-  '-': new SpecialForm((env, evaluate, args) => binOp((x, y) => x - y, evaluate, args)),
-  '/': new SpecialForm((env, evaluate, args) => binOp((x, y) => x / y, evaluate, args)),
+  '+': new SpecialForm((env, evaluate, args) => binOp((x, y) => x + y, exp => evaluate(exp, env), args)),
+  '*': new SpecialForm((env, evaluate, args) => binOp((x, y) => x * y, exp => evaluate(exp, env), args)),
+  '-': new SpecialForm((env, evaluate, args) => binOp((x, y) => x - y, exp => evaluate(exp, env), args)),
+  '/': new SpecialForm((env, evaluate, args) => binOp((x, y) => x / y, exp => evaluate(exp, env), args)),
+
+  '>': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x > y, exp => evaluate(exp, env), args)),
+  '<': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x < y, exp => evaluate(exp, env), args)),
+  '>=': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x >= y, exp => evaluate(exp, env), args)),
+  '<=': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x <= y, exp => evaluate(exp, env), args)),
+  '=': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x === y, exp => evaluate(exp, env), args)),
+  '!=': new SpecialForm((env, evaluate, args) => binCmp((x, y) => x !== y, exp => evaluate(exp, env), args)),
 };
