092ea16ab545f2e38d6dcf937b36d04c502fda77
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-12-22 10:50:45 +0200

    Implemented base code parser

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,48 @@
-const parseSymbol = (code, offset) => {};
+const delimiters = new Set([' ', '\t', '\r', '\n']);
+const listLeftToken = '(';
+const listRightToken = ')';
+const mapLeftToken = '{';
+const mapRightToken = '}';
 
-const parseList = (code, offset) => {};
+module.exports.default = (code) => {
+  let offset = 0;
 
-const parseMap = (code, offset) => {};
+  const currentChar = () => code[offset];
 
-const parseString = (code, offset) => {};
+  const isEof = () => offset >= code.length;
 
-const parseQuotedString = (code, offset) => {};
+  const nextChar = () => offset += 1;
 
-const parseInlineComment = (code, offset) => {};
+  const parseSymbol = () => {};
+
+  const parseList = () => {};
+  
+  const parseMap = () => {};
+
+  const parseCode = () => {
+    const list = [];
+    
+    while (!isEof()) {
+      const char = currentChar();
+      
+      if (delimiters.has(char)) {
+        continue;
+      }
+
+      if (char === listLeftToken) {
+        list.push(parseList());
+      }
+
+      if (char === mapLeftToken) {
+        list.push(parseMap());
+      }
+
+      list.push(parseSymbol());
+    }
+
+    return list;
+  };
+
+  parseCode();
+  
+};
