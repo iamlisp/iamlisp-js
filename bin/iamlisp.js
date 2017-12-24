73d1e45403ddfe5f33d1340d7578d01f270c7229
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-12-24 15:51:07 +0200

    Add repl.

---
+++ bin/iamlisp.js
@@ -0,0 +1,31 @@
+const { createInterface } = require('readline');
+const parse = require('../src/parser');
+const evaluate = require('../src/evaluator').makeEvaluator();
+const print = require('../src/printer');
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stderr,
+  terminal: true,
+});
+
+const readCode = () => new Promise((resolve => {
+  rl.question('> ', input => resolve(input));
+}));
+
+const evalCode = (code) => evaluate(code);
+
+const printResult = (result) => console.log(print(result));
+
+const printError = (err) => console.error(err);
+
+const iter = () => {
+  Promise.resolve()
+    .then(readCode)
+    .then(evalCode)
+    .then(printResult)
+    .catch(printError)
+    .then(iter);
+};
+
+iter();
