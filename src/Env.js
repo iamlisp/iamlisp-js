2c41aad9c3cb3f43e57419ae9ded48cc630d1a05
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-12-24 21:25:17 +0200

    Extract Env to module.

---
+++ src/Env.js
@@ -0,0 +1,32 @@
+module.exports = class Env {
+  constructor(map = new Map(), parent) {
+    this.map = map;
+    this.parent = parent;
+  }
+
+  set(key, value) {
+    this.map.set(key, value);
+  }
+
+  get(key) {
+    if (this.map.has(key)) {
+      return this.map.get(key);
+    }
+
+    if (!this.parent) {
+      throw new Error(`Symbol "${key}" is not defined`);
+    }
+
+    return this.parent.get(key);
+  }
+
+  get entries() {
+    return this.map.entries();
+  }
+
+  import(moduleEnv, moduleName) {
+    for (const [name, value] of moduleEnv.entries) {
+      this.env.set(`${moduleName}/${name}`, value);
+    }
+  }
+}
--- src/evaluator.js
+++ src/evaluator.js
@@ -1,3 +1,4 @@
+const Env = require('./Env');
 const Symbol = require('./Symbol');
 const Lambda = require('./Lambda');
 const Macro = require('./Macro');
@@ -6,29 +7,6 @@ const parse = require('./parser');
 const specialForms = require('./forms');
 const SpecialForm = require('./forms/SpecialForm');
 const { mergeArguments } = require('./util');
-
-class Env {
-  constructor(map = new Map(), parent) {
-    this.map = map;
-    this.parent = parent;
-  }
-
-  set(key, value) {
-    this.map.set(key, value);
-  }
-
-  get(key) {
-    if (this.map.has(key)) {
-      return this.map.get(key);
-    }
-
-    if (!this.parent) {
-      throw new Error(`Symbol "${key}" is not defined`);
-    }
-
-    return this.parent.get(key);
-  }
-}
 
 const getJS = (name) => {
   if (typeof global === 'object') {
