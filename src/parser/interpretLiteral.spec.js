8adb876250b9b90de92be00f719777a1dd58595d
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-06-27 20:51:11 +0300

    Update tests

--- src/parser/interpretLiteral.spec.js
+++ src/parser/interpretLiteral.spec.js
@@ -6,6 +6,7 @@ test.each`
   ${"false"} | ${{ interpreted: true, literal: false }}
   ${"10"}    | ${{ interpreted: true, literal: 10 }}
   ${"3.14"}  | ${{ interpreted: true, literal: 3.14 }}
+  ${"-123"}  | ${{ interpreted: true, literal: -123 }}
   ${"null"}  | ${{ interpreted: true, literal: null }}
   ${"foo"}   | ${{ interpreted: false, literal: undefined }}
   ${"3.1.4"} | ${{ interpreted: false, literal: undefined }}
