3bebe42e94907a5eef4fe2df67b17f5d10687676
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-06-30 21:38:08 +0300

    Fix main module loader

--- src/createEvaluator.js
+++ src/createEvaluator.js
@@ -1,14 +1,15 @@
+import { resolve } from "path";
 import Env from "./Env";
 import print from "./printer/print";
 import evaluate from "./evaluator/evaluate";
 import parse from "./parser/parse";
 import importModule from "./evaluator/importModule";
-import { resolve } from "path";
+import getAppDir from "./helpers/getAppDir";
 
 export default function createEvaluator() {
   const env = new Env();
 
-  importModule(env, resolve("./exts/index.iamlisp"));
+  importModule(env, resolve(getAppDir(), "../exts/index.iamlisp"));
 
   return code => print(evaluate(parse(code), env));
 }
