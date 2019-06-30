b22b54dd4f4bc089f75d616c200e9cae82368ea4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-06-30 21:42:31 +0300

    Fix getAppDir bug

--- src/helpers/getAppDir.js
+++ src/helpers/getAppDir.js
@@ -1,5 +1,5 @@
-import { dirname } from "path";
+import { resolve } from "path";
 
 export default function getAppDir() {
-  return dirname(require.main.filename);
+  return resolve(__dirname, "../");
 }
