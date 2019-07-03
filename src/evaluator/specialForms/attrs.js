5b5ee23bfb1a6cb545c35c42b076e8790c4b152b
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-07-03 15:43:16 +0300

    Update example "brackets-balanced?"

--- src/evaluator/specialForms/attrs.js
+++ src/evaluator/specialForms/attrs.js
@@ -2,16 +2,16 @@ import SpecialForm from "../../types/SpecialForm";
 import { evaluateExpression } from "../evaluate";
 
 const attrForms = {
-  "has-attr": new SpecialForm((env, [obj, name]) => {
+  has: new SpecialForm((env, [obj, name]) => {
     return evaluateExpression(name, env) in evaluateExpression(obj, env);
   }),
-  "del-attr": new SpecialForm((env, [obj, name]) => {
+  delete: new SpecialForm((env, [obj, name]) => {
     delete evaluateExpression(obj, env)[evaluateExpression(name, env)];
   }),
-  "get-attr": new SpecialForm((env, [obj, name]) => {
+  get: new SpecialForm((env, [obj, name]) => {
     return evaluateExpression(obj, env)[evaluateExpression(name, env)];
   }),
-  "set-attr": new SpecialForm((env, [obj, name, value]) => {
+  set: new SpecialForm((env, [obj, name, value]) => {
     evaluateExpression(obj, env)[
       evaluateExpression(name, env)
     ] = evaluateExpression(value, env);
