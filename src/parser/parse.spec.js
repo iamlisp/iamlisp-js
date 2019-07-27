cebc58e109993d27a640c380dd344f80f86049e7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-07-27 22:49:36 +0300

    Refactoring parser tests

--- src/parser/parse.spec.js
+++ src/parser/parse.spec.js
@@ -1,32 +1,33 @@
+import { first } from "lodash";
 import parse from "./parse";
 import Symbl from "../types/Symbl";
 import DotPunctuator from "../types/DotPunctuator";
+import Keyword from "../types/Keyword";
 
-// Test parser
 test.each`
-  input                            | output
-  ${"foo"}                         | ${[new Symbl("foo")]}
-  ${"foo bar"}                     | ${[new Symbl("foo"), new Symbl("bar")]}
-  ${'"hello" 123 true false null'} | ${["hello", 123, true, false, null]}
-  ${'"\\"1\\""'}                   | ${[`"1"`]}
-  ${"(list 1 2 3 4)"}              | ${[[new Symbl("list"), 1, 2, 3, 4]]}
-  ${"(+ 1 2 (- 10 5))"}            | ${[[new Symbl("+"), 1, 2, [new Symbl("-"), 10, 5]]]}
-  ${"(+ 1 2) (- 10 5)"}            | ${[[new Symbl("+"), 1, 2], [new Symbl("-"), 10, 5]]}
-  ${`{foo 1 "bar" 2}`}             | ${[{ foo: 1, bar: 2 }]}
-  ${`(foo . bar)`}                 | ${[[new Symbl("foo"), DotPunctuator.INSTANCE, new Symbl("bar")]]}
-  ${`(foo .bar)`}                  | ${[[new Symbl("foo"), new Symbl(".bar")]]}
-`('Should correclty parse "$input"', ({ input, output }) => {
-  expect(parse(input)).toEqual(output);
+  type                         | input                      | output
+  ${"symbol"}                  | ${"foo"}                   | ${new Symbl("foo")}
+  ${"integer"}                 | ${"1234"}                  | ${1234}
+  ${"negative integer"}        | ${"-1234"}                 | ${-1234}
+  ${"float"}                   | ${"12.34"}                 | ${12.34}
+  ${"negative float"}          | ${"-12.34"}                | ${-12.34}
+  ${"exponential number"}      | ${"10e-4"}                 | ${0.001}
+  ${"boolean:true"}            | ${"true"}                  | ${true}
+  ${"boolean:false"}           | ${"false"}                 | ${false}
+  ${"undefined"}               | ${"undefined"}             | ${undefined}
+  ${"null"}                    | ${"null"}                  | ${null}
+  ${"keyword"}                 | ${":kw"}                   | ${Keyword.for("kw")}
+  ${"string"}                  | ${'"eeny meeny miny moe"'} | ${"eeny meeny miny moe"}
+  ${"empty list"}              | ${"()"}                    | ${[]}
+  ${"list with content"}       | ${"(1 2 (3 4 5))"}         | ${[1, 2, [3, 4, 5]]}
+  ${"empty map"}               | ${"{}"}                    | ${new Map()}
+  ${"map with keyword as key"} | ${'{:foo "bar"}'}          | ${new Map([[Keyword.for("foo"), "bar"]])}
+  ${"map with string as key"}  | ${'{"foo" "bar"}'}         | ${new Map([["foo", "bar"]])}
+  ${"map with symbol as key"}  | ${'{foo "bar"}'}           | ${new Map([[new Symbl("foo"), "bar"]])}
+  ${"quoted expression"}       | ${"'(1 2 3)"}              | ${[new Symbl("quote"), [1, 2, 3]]}
+  ${"expression with meta"}    | ${'^{:foo "bar"}(1 2 3)'}  | ${[new Symbl("with-meta"), new Map([[Keyword.for("foo"), "bar"]]), [1, 2, 3]]}
+  ${"empty inline lambda"}     | ${"#()"}                   | ${[new Symbl("lambda"), [DotPunctuator.INSTANCE, new Symbl("%&")], []]}
+  ${"inline lambda"}           | ${"#(/ (+ %1 %2) 2)"}      | ${[new Symbl("lambda"), [new Symbl("%1"), new Symbl("%2"), DotPunctuator.INSTANCE, new Symbl("%&")], [new Symbl("/"), [new Symbl("+"), new Symbl("%1"), new Symbl("%2")], 2]]}
+`("Should currectly parse $input as $type", ({ input, output }) => {
+  expect(first(parse(input))).toEqual(output);
 });
-
-// Test plugin "placeholder"
-test.each`
-  input          | output
-  ${"(+ _ _ _)"} | ${[[new Symbl("lambda"), [new Symbl("__0"), new Symbl("__1"), new Symbl("__2")], [new Symbl("+"), new Symbl("__0"), new Symbl("__1"), new Symbl("__2")]]]}
-  ${"(- _ _)"}   | ${[[new Symbl("lambda"), [new Symbl("__0"), new Symbl("__1")], [new Symbl("-"), new Symbl("__0"), new Symbl("__1")]]]}
-`(
-  'Should correclty parse "$input" with plugin "placeholder"',
-  ({ input, output }) => {
-    expect(parse(input)).toEqual(output);
-  }
-);
