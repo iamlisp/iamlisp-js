ce82fee2b720fd39379f75be63f66822be209e02
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-10-11 00:44:08 +0300

    Refactoring test

--- src/tests/challenges/treasure-hunter.spec.js
+++ src/tests/challenges/treasure-hunter.spec.js
@@ -11,34 +11,37 @@ test("Treasure Hunter", () => {
     (import "list" as "l")
 
     (def initial-coords (l/list 1 1))
+    (def map-width 5)
+    (def input-map-data
+      (l/list
+        55 14 25 52 21
+        44 31 11 53 43
+        24 13 45 12 34
+        42 22 43 32 41
+        51 23 33 54 15))
 
     (defun find-treasure (map-data) 
-      (def treasure-map (l/chunk 5 map-data))
+      (def treasure-map (-> map-data (l/chunk map-width)))
 
       (defun iter (coords acc)
         (def y (-> coords (l/nth 0))
              x (-> coords (l/nth 1)))
 
-        (def hint (-> treasure-map (l/nth (dec y))
+        (def clue (-> treasure-map (l/nth (dec y))
                                    (l/nth (dec x))))
 
-        (def hint-y (floor (/ hint 10))
-             hint-x (% hint 10))
+        (def clue-y (floor (/ clue 10))
+             clue-x (% clue 10))
              
         (def next-path (l/list* coords acc))
 
-        (cond ((and (eq? y hint-y) (eq? x hint-x)) next-path)
-              ((iter (l/list hint-y hint-x) next-path))))
+        (cond ((and (eq? y clue-y) 
+                    (eq? x clue-x)) next-path)
+              ((iter (l/list clue-y clue-x) next-path))))
 
       (l/reverse (iter initial-coords l/Nil)))
 
-    (find-treasure
-      (l/list
-          55 14 25 52 21
-          44 31 11 53 43
-          24 13 45 12 34
-          42 22 43 32 41
-          51 23 33 54 15))
+    (find-treasure input-map-data)
   `;
 
   expect(evaluate(code)).toBe(
